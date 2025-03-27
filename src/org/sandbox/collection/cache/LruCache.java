b31db1f0d91f6fca4bba389e2a31d3085a5bf77f
Josu Martinez <josu.martinez@gmail.com>
2025-03-28 00:57:52 +0100

    docs: add documentation to LRU cache

--- src/org/sandbox/collection/cache/LruCache.java
+++ src/org/sandbox/collection/cache/LruCache.java
@@ -5,19 +5,30 @@ import java.util.Map;
 import java.util.Optional;
 import org.sandbox.collection.cache.DoubleLinkedList.Node;
 
+/**
+ * Models a Least Recently Used (LRU) eviction cache. This means that whenever the maximum
+ * capacity of the cache is hit, then the least recently used item is removed from the cache.
+ * The invocation of both <code>get</code> and <code>put</code> methods result in the input
+ * item to be considered the most recently used item.
+ */
 public final class LruCache<K, V> implements Cache<K, V> {
 
-    private final int capacity;
-    private final Map<K, Node<K, V>> cache; // Enables item access
-    private final DoubleLinkedList<K, V> list;
+    private final int capacity; // Maximum cache item capacity
+    private final Map<K, Node<K, V>> cache; // Enables O(1) item access
+    private final DoubleLinkedList<K, V> list; // Enables O(1) item insertion and eviction
 
     public LruCache(final int capacity) {
-        if (capacity < 1) throw new IllegalArgumentException();
+        if (capacity < 1) {
+            throw new IllegalArgumentException(
+                "The cache must be able to contain at least one item"
+            );
+        }
         this.capacity = capacity;
         this.cache = new HashMap<>();
         this.list = new DoubleLinkedList<>();
     }
 
+    @Override
     public Optional<V> get(final K key) {
         Node<K, V> node = cache.get(key);
         if (node != null) {
@@ -28,6 +39,7 @@ public final class LruCache<K, V> implements Cache<K, V> {
         return Optional.empty();
     }
 
+    @Override
     public void put(final K key, final V value) {
         Node<K, V> node = cache.get(key);
         if (node != null) {
@@ -45,10 +57,12 @@ public final class LruCache<K, V> implements Cache<K, V> {
         }
     }
 
+    @Override
     public int size() {
         return list.size();
     }
 
+    @Override
     public boolean isEmpty() {
         return list.isEmpty();
     }
